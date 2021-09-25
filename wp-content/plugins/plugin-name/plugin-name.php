cd3134ae7ec19f8df6a00b13eb0c3aa536faac13
unknown <sajeel.ahmed@protonmail.com>
2021-09-25 15:16:08 +0500

    :new: Add License

--- wp-content/plugins/plugin-name/plugin-name.php
+++ wp-content/plugins/plugin-name/plugin-name.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /**
@@ -11,4 +12,19 @@ Description: This is just for learning purposes
 Author: Sajeel Ahmad
 Version: 1.0.0
 Author URI: https://github.com/sajeell
+*/
+
+/*
+This is a free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+This is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with it. If not, see https://github.com/sajeel/plugin-practice.
 */
