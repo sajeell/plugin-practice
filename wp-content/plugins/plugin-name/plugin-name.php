60a39ef636edb590ce940213cf9aaea1eda729c1
unknown <sajeel.ahmed@protonmail.com>
2021-09-25 15:07:40 +0500

    :new: Add Header

---
+++ wp-content/plugins/plugin-name/plugin-name.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package First_Plugin
+ * @version 1.0.0
+ */
+/*
+Plugin Name: First Plugin
+Plugin URI: https://github.com/sajeell/plugin-practice
+Description: This is just for learning purposes
+Author: Sajeel Ahmad
+Version: 1.0.0
+Author URI: https://github.com/sajeell
+*/
